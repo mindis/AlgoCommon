13de33a5a500bff8020fabb4d8ffb124633f25e6
Zhenzhong Xu <flyfree@gmail.com>
2014-08-06 22:55:06 -0700

    array ops

---
+++ AlgoCommon/Array/ArrayOps.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace AiDriven.AlgoCommon.Array
+{
+    public static class ArrayOps
+    {
+        /// <summary>
+        ///  Given a sorted array, remove the duplicates in place such that each element appear only once
+        ///  and return the new length.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int RemoveDups(int[] array)
+        {
+            if (array == null)
+            {
+                return 0;
+            }
+
+            if (array.Length == 1)
+            {
+                return 1;
+            }
+
+            int currentIndex = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[currentIndex] != array[i])
+                {
+                    array[++currentIndex] = array[i];
+                }
+            }
+            return ++currentIndex;
+        }
+
+
+        /// <summary>
+        ///  numbers on the row below.
+        /// For example, given the following triangle
+        /// [
+        ///     [2],
+        ///    [3,4],
+        ///   [6,5,7],
+        ///  [4,1,8,3]
+        // ]
+        //The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+        //Note: Bonus point if you are able to do this using only O(n) extra space, where n is the total number of
+        //rows in the triangle.
+        /// </summary>
+        /// <param name="triangle"></param>
+        /// <returns></returns>
+        public static int FindTriangleMinimumPathSum(int[][] triangle)
+        {
+            // no parameter validation to save time, assuming parameter is a valid triangle
+            int numberOfRolws = triangle.Length;
+
+            int currentPathSum = triangle[0][0];
+            for (int i = triangle.Length-2; i >= 0; i--)
+            {
+                for (int j = 0; j < triangle[i].Length; j++)
+                {
+                    triangle[i][j] += Math.Min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+                }
+            }
+
+            return triangle[0][0];
+        }
+    }
+}
--- AlgoCommon/Runner/Program.cs
+++ AlgoCommon/Runner/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace AiDriven.AlgoCommon.Runner
+﻿using System;
+using System.Diagnostics;
+
+namespace AiDriven.AlgoCommon.Runner
 {
+    using AiDriven.AlgoCommon.Array;
     class Program
     {
         static void Main(string[] args)
         {
+            TestArrayRemoveDups();
+            TestTriangleMinPathSum();
+        }
+
+
+        public static void TestArrayRemoveDups()
+        {
+            int[] array = new[] { 1, 2, 2, 2, 3, 4, 5, 5, 8 };
+            int length = ArrayOps.RemoveDups(array);
+            Debug.Assert(length == 6);
+            Debug.Assert(array[0] == 1);
+            Debug.Assert(array[1] == 2);
+            Debug.Assert(array[2] == 3);
+            Debug.Assert(array[3] == 4);
+            Debug.Assert(array[4] == 5);
+            Debug.Assert(array[5] == 8);
+                 
+        }
+
+        public static void TestTriangleMinPathSum()
+        {
+            int[][] triangle = new[]
+            {
+                new[] {2},
+                new[] {3, 4},
+                new[] {6, 5, 7},
+                new[] {4, 1, 8, 3}
+            };
+
+            int result = ArrayOps.FindTriangleMinimumPathSum(triangle);
+            Debug.Assert(result == 11);
         }
     }
 }
